900b46ad41b7cdd0e045b58b026deab61175767e
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-22 20:03:09 +0300

    I done this challenge

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Good Luck 💪🏾
+function mostExpensive(jewelry) {
+    let mostExpensiveItem = '';
+    let highestPrice = 0
+    for (let item in jewelry) {
+        if (jewelry[item] > highestPrice) {
+            highestPrice = jewelry[item];
+            mostExpensiveItem = [item];
+        }
+    }
+    
+     return  `The most expensive one is the ${mostExpensiveItem}`;
+}
+
+let mh= mostExpensive({
+  "Diamond Earrings": 980,
+  "Gold Watch": 250,
+  "Pearl Necklace": 4650
+}); 
+console.log(mh)
+console.log(mostExpensive({
+  "Silver Bracelet": 280,
+  "Gemstone Earrings": 180,
+  "Diamond Ring": 3500
+})); 
+
